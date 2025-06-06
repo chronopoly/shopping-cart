c9134b7fc9ace69c4a67fd8908c4caae9d13cf16
poly <206082275+chronopoly@users.noreply.github.com>
2025-06-06 12:35:24 +0300

    Show total order cost in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,17 @@
 import ProductCard from "../ProductCard/ProductCard";
 
 const Cart = ({ items, handleCartUpdate, removeFromCart }) => {
+  let total = 0;
+  items.forEach(item => {
+    total += item.quantity * item.price;
+  });
+
   return (
     <div>
       <h1>This is a cart</h1>
+      <h2>Total cost: ${total} </h2>
       {items.map(item => <ProductCard info={item} handleCartUpdate={handleCartUpdate} removeFromCart={removeFromCart} type="cart" />)}
+      
     </div>
   )
 };
