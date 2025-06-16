958537a7493b8a86f3c153e16f92747faf1203fb
poly <206082275+chronopoly@users.noreply.github.com>
2025-06-16 11:04:31 +0300

    Add some background colors

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
-import './App.css'
+import styles from './App.module.css'
 import Navbar from '../Navbar/Navbar'
 import Products from '../Products/Products'
 import Cart from '../Cart/Cart'
