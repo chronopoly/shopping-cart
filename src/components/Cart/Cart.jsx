35371a22eb44d1d1a65e533232c05c6e9ec80fac
poly <206082275+chronopoly@users.noreply.github.com>
2025-06-17 15:02:39 +0300

    Add and style the payment section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -16,17 +16,26 @@ const Cart = ({ items, handleCartUpdate, removeFromCart }) => {
       <div className={styles.checkout}>
         <div className={styles.summary}>
           <div className={styles.coupon}>
-            <h1>Discount Coupon</h1>
+            <h2>Discount Coupon</h2>
             <input type="text" placeholder="Enter your coupon code"/>
             <button>Apply your discount</button>
           </div>
-          <h1>Order Summary</h1>
+          <h2>Order Summary</h2>
           <p>Discount: <span>$0.0</span></p>
           <p>Delivery: <span>$49.99</span></p>
           <p>Total: <span className={styles.total}>${(total + 49.99).toFixed(2)}</span></p>
         </div>
         <div className={styles.payment}>
-          Pay here
+          <h2>Payment Method</h2>
+          <div>
+            <input type="radio" name="payment" value="cash" id="paymentChoice1" />
+            <label htmlFor="paymentChoice1">Cash</label>
+
+            <input type="radio" name="payment" value="cash" id="paymentChoice2" />
+            <label htmlFor="paymentChoice2">Credit Card</label>
+
+          </div>
+          <button>Check Out</button>
         </div>
         
       </div>
